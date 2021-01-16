b59c28b074a6348df8f0462a56abd3d85fcb31ec
nmago <e-five@mail.ru>
2021-01-16 17:05:49 +0300

    Revert "Allow frontend requests"
    This reverts commit 362e2bc7b64db95e7d547078ded13b76d6969d6f.

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,6 @@
 from typing import List
 
 from fastapi import Depends, FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
 import crud
@@ -14,17 +13,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 # Dependency
 def get_db():
