90e0d9ff8d83d3be787cd5110aee27d63faa6b62
nmago <e-five@mail.ru>
2021-01-16 22:15:53 +0300

    Support react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './assets/css/App.css';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
@@ -8,10 +9,19 @@ import UnitFilter from './components/UnitFilter';
 function App() {
   return (
     <div className="container-fluid">
-      <NavBar/>
-      <PageWithContent title="Наши специалисты" name="Специалисты">
-        <UnitFilter />
-      </PageWithContent>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <PageWithContent title="Наши специалисты" name="Специалисты">
+            <UnitFilter />
+          </PageWithContent>
+        </Route>
+        <Route exact path="/about">
+          <PageWithContent title="О нас" name="О нас">
+            <p className="alert alert-info">Информацию о проекте можно получить в группе.</p>
+          </PageWithContent>
+        </Route>
+      </Switch>
 
       <Footer />
     </div>
