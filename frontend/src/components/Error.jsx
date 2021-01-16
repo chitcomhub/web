f88bcb525250db6f6cce0936919b6158aef55fb6
nmago <e-five@mail.ru>
2021-01-16 19:14:49 +0300

    Fix warning stuff

--- frontend/src/components/Error.jsx
+++ frontend/src/components/Error.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Error = (props) => {
   return (
-    <div class="alert alert-danger" role="alert">
-        <h4 class="alert-heading">Произошла фатальная ошибка!</h4>
-        <p>{props.text}</p>
+    <div className="alert alert-danger" role="alert">
+      <h4 className="alert-heading">Произошла фатальная ошибка!</h4>
+      <p>{props.text}</p>
     </div>
   );
 };
